74ffe12e95bc80e61ada24e474be6b731a6544ca
Sergio <sergio@panaghia.it>
2011-09-03 14:45:41 +0200

    fixed a bug causing overflowing scrolling right

--- Source/swipeable.js
+++ Source/swipeable.js
@@ -6,7 +6,9 @@ Swipeable = function(scrollerId)
 	this.scroller = document.getElementById(scrollerId);
 	
 	this.singleWidth = 0;
-	this.totalWidth = 0;
+	this.totalWidth = 0;   
+	
+	this.slotsNumber = 0;
 		
 	this.calculateContainerWidth();
 	this.attachEvents();
@@ -17,7 +19,8 @@ Swipeable.prototype.calculateContainerWidth = function()
 {
 	var children = this.scroller.querySelectorAll('.risultatiPage');  
 	this.singleWidth = getComputedStyle(children[0], "").getPropertyValue("width").match(/(\d*\.?\d*)(.*)/)[1];
-	this.totalWidth = this.singleWidth * children.length;
+	this.totalWidth = this.singleWidth * children.length;  
+	this.slotsNumber = children.length;
 	this.scroller.style.width = this.totalWidth+"px";
 }
 
@@ -46,7 +49,6 @@ Swipeable.prototype.attachEvents = function()
 	{
 		e.stopPropagation();		
 		yPos = e.touches[0].pageY;
-		xPos = startX;
 		deltaX = e.touches[0].pageY;
 		xPos = startX;
 		deltaX = e.touches[0].pageX - startX;
@@ -57,12 +59,12 @@ Swipeable.prototype.attachEvents = function()
 	{
 		deltaY = e.touches[0].pageY - yPos;    	
 		var delta = e.touches[0].pageX - deltaX;
-		
+		/*
 		console.log("deltay:"+Math.abs(deltaY)+" deltax:"+Math.abs(delta));
-		console.log(lock);
+		console.log(lock);      */
 		
 		Math.abs(deltaY) > (Math.abs(delta) + 2) ? lock = true : lock = false;		 
-		delta = parseInt(delta/2);
+		delta = parseInt(delta/1.5);
 		startX = endX = delta;
 		if(!lock)
 		{ 		
@@ -81,31 +83,34 @@ Swipeable.prototype.attachEvents = function()
 		function restoreState()
 		{
 		    scroller.style.webkitTransition = 'all 300ms ease-in-out';
-			scroller.style.webkitTransform = 'translate3d('+absX+'px, 0, 0)';
-			startX = 0;	   
+			scroller.style.webkitTransform = 'translate3d('+off+'px, 0, 0)';
+			startX = 0;      
 		}
 		
 		var finalDelta = xPos - endX; 
-		
+		console.log('current offset: '+off);
 		if(finalDelta < 0 && !lock)
 		{
 			if(finalDelta < -50 && off < 0)
 			{
 				scroller.style.webkitTransition = 'all 200ms ease-in-out';
 				off+=parseInt(that.singleWidth);
+				console.log('new offset < 0: '+off);
 				scroller.style.webkitTransform = 'translate3d('+off+'px, 0, 0)';   			
-				absX = -off;
-				startX = 0;      			
+				absX = off;
+				startX = 0;      			 			
 			}
 			else
 				restoreState();
 		}
 		else if(finalDelta > 0)
-		{                     
-			if(finalDelta > 50 && off >= 0)
+		{                                 
+			var deadline = (that.slotsNumber-2) * that.singleWidth;
+			if(finalDelta > 50 && off >= -deadline)
 			{
 				scroller.style.webkitTransition = 'all 200ms ease-in-out';
 				off-=parseInt(that.singleWidth);
+				console.log('new offset >=0: '+off);
 				scroller.style.webkitTransform = 'translate3d('+off+'px, 0, 0)';     			
 				absX = off; 
 				startX = 0; 	
